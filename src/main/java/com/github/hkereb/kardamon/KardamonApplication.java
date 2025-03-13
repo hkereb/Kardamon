d0723600cbb4be9919d8935215b6fb5a1cbae83d
hkereb <115366640+hkereb@users.noreply.github.com>
2025-03-13 15:45:46 +0100

    Initial commit

---
+++ src/main/java/com/github/hkereb/kardamon/KardamonApplication.java
@@ -0,0 +1,13 @@
+package com.github.hkereb.kardamon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KardamonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KardamonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hkereb/kardamon/KardamonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hkereb.kardamon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KardamonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
