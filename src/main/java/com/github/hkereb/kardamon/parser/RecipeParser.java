acf3716622412a5bab1d6d2337cbeabbb2152750
hkereb <115366640+hkereb@users.noreply.github.com>
2025-03-16 22:39:11 +0100

    parsing for JSON-LD type recipes

--- src/main/java/com/github/hkereb/kardamon/parser/RecipeParser.java
+++ src/main/java/com/github/hkereb/kardamon/parser/RecipeParser.java
@@ -8,76 +8,79 @@ import org.json.JSONObject;
 import java.util.*;
 import java.util.regex.Pattern;
 
+// TODO split to smaller parsers
 public class RecipeParser {
     private final Document doc;
     public RecipeParser(Document doc) {
         this.doc = doc;
     }
-    private static final Pattern SERVINGS_PATTERN = Pattern.compile("(?i)(makes|serves|servings|yield|liczba\\s+porcji|porcje|porcja|porcji|os\\.)\\s*:?\\\\s*(\\d+)\n");
-
     public JSONObject parseRecipe() {
-        JSONObject recipeJson = new JSONObject();
-        JSONObject jsonLdObject = getJsonLDObject();
+        ///// JSON-LD ///////////////////////////////////////////////////////////////////////////////////////////////
+        JSONArray jsonLdObjects = getJsonLDObjects();
+        if (!jsonLdObjects.isEmpty()) {
+            for (int i = 0; i < jsonLdObjects.length(); i++) {
+                JSONObject jsonLdObject = jsonLdObjects.getJSONObject(i);
+                JSONObject parsed = parseJsonLd(jsonLdObject);
 
-        if (jsonLdObject != null) {
-            JSONObject parsed = parseJsonLd(jsonLdObject);
-            if (!parsed.getJSONArray("ingredients").isEmpty()) {
-                return parsed;
+                if (!parsed.getJSONArray("ingredients").isEmpty()) {
+                    return parsed;
+                }
             }
         }
-
+        ///// MICRODATA ///////////////////////////////////////////////////////////////////////////////////////////////
         if (hasMicrodata()) {
             return parseMicrodata();
         }
-
-        // Jeśli nic nie znaleźliśmy, stosujemy awaryjne parsowanie na podstawie heurystyk
+        ///// UNSTRUCTURED ////////////////////////////////////////////////////////////////////////////////////////////
         //return parseFallback();
         return new JSONObject();
     }
 
-    private String getServings() {
-        for (Element element : doc.getAllElements()) {
-            String text = element.text().trim();
-            var matcher = SERVINGS_PATTERN.matcher(text);
-            if (matcher.find()) {
-                return matcher.group(2);
-            }
-        }
-        return "";
-    }
-    private JSONObject getJsonLDObject() {
-        Element jsonLdScript = doc.selectFirst("script[type='application/ld+json']");
-        if (jsonLdScript != null) {
+
+
+    ///// JSON-LD ///////////////////////////////////////////////////////////////////////////////////////////////
+    private JSONArray getJsonLDObjects() {
+        Elements jsonLdScripts = doc.select("script[type='application/ld+json']"); // Zmieniamy na select(), aby uzyskać wszystkie tagi
+        JSONArray jsonObjects = new JSONArray();
+
+        for (Element jsonLdScript : jsonLdScripts) {
             try {
-                return new JSONObject(jsonLdScript.html());
+                JSONObject jsonObject = new JSONObject(jsonLdScript.html());
+                jsonObjects.put(jsonObject);
             } catch (Exception e) {
-                System.err.println("Cannot parse JsonLD" + e.getMessage());
+                System.err.println("Cannot parse JsonLD: " + e.getMessage());
             }
         }
-        return null;
+
+        return jsonObjects;
     }
-    private JSONObject parseJsonLd(JSONObject jsonObject) {
+    private JSONObject parseJsonLd(JSONObject jsonLDObject) {
         JSONObject recipeJson = new JSONObject();
 
         recipeJson.put("title", getTitle());
         recipeJson.put("description", getDescription());
-        recipeJson.put("servings", jsonObject.optString("recipeYield", ""));
-        recipeJson.put("ingredients", extractJsonArray(jsonObject, "recipeIngredient"));
-        recipeJson.put("instructions", extractInstructions(jsonObject));
+        recipeJson.put("servings", extractSingleValue(jsonLDObject, "recipeYield"));
+        recipeJson.put("ingredients", extractJsonArray(jsonLDObject, "recipeIngredient"));
+        recipeJson.put("instructions", extractInstructions(jsonLDObject));
 
         return recipeJson;
     }
-    private List<String> extractJsonArray(JSONObject jsonObject, String key) {
-        List<String> result = new ArrayList<>();
+    private String extractSingleValue(JSONObject jsonObject, String key) {
         if (jsonObject.has(key)) {
-            JSONArray array = jsonObject.getJSONArray(key);
-            for (Object item : array) {
-                result.add(item.toString());
+            return jsonObject.getJSONArray(key).getString(0);
+        }
+        return "";
+    }
+    private List<String> extractJsonArray(JSONObject jsonLDObject, String key) {
+        List<String> result = new ArrayList<>();
+        if (jsonLDObject.has(key)) {
+            JSONArray array = jsonLDObject.getJSONArray(key);
+            for (int i = 0; i < array.length(); i++) {
+                result.add(array.getString(i));
             }
         }
         return result;
     }
-
     private List<String> extractInstructions(JSONObject jsonObject) {
         List<String> instructions = new ArrayList<>();
         if (jsonObject.has("recipeInstructions")) {
@@ -94,10 +97,10 @@ public class RecipeParser {
         return instructions;
     }
 
+    ///// MICRODATA ///////////////////////////////////////////////////////////////////////////////////////////////
     private boolean hasMicrodata() {
         return !doc.select("[itemprop]").isEmpty();
     }
-
     private JSONObject parseMicrodata() {
         JSONObject recipeJson = new JSONObject();
 
@@ -109,18 +112,6 @@ public class RecipeParser {
 
         return recipeJson;
     }
-
-    private String extractServings() {
-        for (Element element : doc.getAllElements()) {
-            String text = element.text().trim();
-            var matcher = SERVINGS_PATTERN.matcher(text);
-            if (matcher.find()) {
-                return matcher.group(2);
-            }
-        }
-        return "";
-    }
-
     private List<String> extractMicrodataList(String itemprop) {
         List<String> results = new ArrayList<>();
         Elements elements = doc.select("[itemprop='" + itemprop + "']");
@@ -140,11 +131,13 @@ public class RecipeParser {
         }
         return "";
     }
+
+    ///// UNSTRUCTURED ////////////////////////////////////////////////////////////////////////////////////////////
     private JSONObject parseFallback() {
         JSONObject recipeJson = new JSONObject();
         recipeJson.put("title", getTitle());
         recipeJson.put("description", getDescription());
-        recipeJson.put("servings", extractServings());
+        //recipeJson.put("servings", extractServings());
         recipeJson.put("ingredients", Collections.emptyList());
         recipeJson.put("instructions", Collections.emptyList());
         return recipeJson;
@@ -153,7 +146,6 @@ public class RecipeParser {
         Element ogTitle = doc.selectFirst("meta[property=og:title]");
         return (ogTitle != null) ? ogTitle.attr("content") : doc.title();
     }
-
     private String getDescription() {
         Element ogDescription = doc.selectFirst("meta[property=og:description]");
         if (ogDescription != null) return ogDescription.attr("content");
